cdac5ec64e1aa16aead2086dd3be76614cc26739
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2017-03-29 13:09:02 -0300

    0.6.0

--- _js/main.js
+++ _js/main.js
@@ -1,3 +1,4 @@
+// Debugging
 var debugFrames = document.querySelector('#debugFrames');
 var debugExplode = document.querySelector('#debugExplode');
 var debugCenter = document.querySelector('#debugCenter');
@@ -16,7 +17,30 @@ debugFrames.addEventListener('click', debugFramesUpdate);
 debugExplode.addEventListener('click', debugExplodeUpdate);
 debugCenter.addEventListener('click', debugCenterUpdate);
 
+// Scroll to center
 $(function() {
   $(".parallax").scrollTop(350);
   $(".parallax").scrollLeft(600);
+})
+
+// Loading
+$(function() {
+  $('#loading #top').animate({right:'0px'}, 200, function() {
+    $('#loading #right').animate({bottom:'0px'}, 200, function() {
+      $('#loading #bottom').animate({left:'0px'}, 200, function() {
+        $('#loading #left').animate({top:'0px'}, 200, function() {
+          $('#loading #wait').animate({opacity: '0'}, 900, function() {
+            $('#loading #wait').css({display: 'none'});
+          });
+        });
+      });
+    });
+  });
+})
+
+// Popups
+$(function() {
+  $('.project h1').click(function() {
+    $(this).parent().children('.popup').css({display: 'block'});
+  })
 })
